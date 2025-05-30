e2863e42be28f02ed2a838f583624edef22de51b
zzong_0 <83694815+framech01@users.noreply.github.com>
2025-05-30 21:21:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from regional_risk_index import analyze_regional_risk
 from vehicle_risk_clustering import train_vehicle_model
 from future_risk_forecasting import run_prophet_analysis
 
-DATA_PATH = "C:/Users/cjfwh/Downloads/최종_병합_교통사고_차량등록.csv"
+DATA_PATH = "-"
 
 print("[1] Regional Risk Index Analysis")
 analyze_regional_risk(DATA_PATH)
